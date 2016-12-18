bac8b43d83b615df155db9d7cb891073a0367c60
cherylcho <ccho31@gmail.com>
2016-12-18 15:15:08 -0500

    dark sky api + goog calendar api
    dark sky api and google calendar api pulled in. google authorization added in order to pull calendar information

--- javascript/main.old.js
+++ javascript/main.old.js
@@ -13,26 +13,37 @@ function getQueryParams() {
     }, {});
 }
 
+const googKey = 'AIzaSyAcI0JUkfni3Do4HC9uf1d7LIK9_ZGIj0g';
+const darkSkyKey = 'c2a8e9fddd67de169268702d428a554f';
+
+
 // 790796644928-q7g8382df247mb9m2bmdt09m3s5qu3sh.apps.googleusercontent.com
 // IKJdJw0J1QP7giJt_84jorVk -- s
 
 // http://darksky.webscript.io/forecast?key=c2a8e9fddd67de169268702d428a554f&latLng=40.7595044,-73.9271643999999
 
 function getWeather(zipcode) {
-	const darkSkyKey = 'c2a8e9fddd67de169268702d428a554f';
-	new google.maps.Geocoder().geocode({
-        'address': zipcode
+    console.log(zipcode)
+
+    var lat = '';
+    var lng = '';
+    var address = zipcode
+    new google.maps.Geocoder().geocode({
+        'address': address
     }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            const lat = results[0].geometry.location.lat();
-            const lng = results[0].geometry.location.lng();
+            lat = results[0].geometry.location.lat();
+            lng = results[0].geometry.location.lng();
+            console.log('Latitude: ' + lat + ' Logitude: ' + lng);
 
-            const urlToRequest = `http://darksky.webscript.io/forecast?key=${darkSkyKey}&latLng=${lat},${lng}`;
+            const urlToRequest = `http://darksky.webscript.io/forecast?key=c2a8e9fddd67de169268702d428a554f&latLng=${lat},${lng}`;
+            console.log(urlToRequest)
+
             const data = $.get(urlToRequest);
+
             data.then(onDataBack);
         } else {
-            alert("Geocode was not successful for the following reason: " + status + '. Redirecting you to homepage');
-            window.location.href = 'index.html';
+            alert("Geocode was not successful for the following reason: " + status);
         }
     });
 
@@ -40,12 +51,17 @@ function getWeather(zipcode) {
 
 function onDataBack(response) {
     const data = JSON.parse(response);
+    console.log(data)
 
     const degreesFarenheit = data.currently.temperature;
 
     const chancePrecip = data.hourly.data[0].precipProbability;
     const percentPrecip = Math.round((chancePrecip * 100) / 10);
     const precipType = data.hourly.data[0].precipType;
+    console.log(percentPrecip);
+    console.log(precipType);
+
+    console.log('-p------', precipType)
 
 
     const weatherObjects = [];
@@ -94,14 +110,10 @@ function onDataBack(response) {
 	`;
     });
 
-    $('.js-container').append(weatherCards.join('\n'));
+    console.log(weatherCards);
 
-}
 
-function displayCalendarEvents(events) {
-	console.log(events)
-
-const eventObjects = [{
+    const eventObjects = [{
         imageSrc: 'assets/dress.jpg',
         name: 'Holiday Party',
         time: '7pm',
@@ -143,144 +155,113 @@ const eventObjects = [{
 	`;
     });
 
+    console.log(weatherCards);
 
-$('.js-container').append(eventCards.join('\n'));
+    $('.js-container').html(weatherCards.join('\n') + eventCards.join('\n'));
+
 }
 
-// THIS METHOD IS RUN BY THE GOOGLE APIS CALL
-function initAppFromGoogleScript() {
+function checkForZipcode() {
     const isPlanPage = $('.js-plan').length > 0;
 
     if (!isPlanPage) {
         return; // stop this funcion, we are done
     }
 
-    // on plan page, print out today's date
-    getDateAsText();
-
-    //on plan page, check for zipcode
+    // otherwise, we are on plan page, check for zipcode
     const queryParams = getQueryParams();
-
     // write an if statement that checks to see if zipcode exists
+
     if (typeof queryParams.zipcode === 'undefined') {
         window.location.href = 'index.html';
     } else {
-    	// do api stuff
-    	// load weather regardless
-    	getWeather(queryParams.zipcode)
-    	// get google permissions and *then* load calendar info
-    	getGoogleCalAuth(true)
-    		.then(() => loadCalendarData())
-    		.then((events) => displayCalendarEvents(events))
-    		.catch(() => {
-    			// handle failure here :/
-    			$('.js-container').append(`
-<div class="ui centered card clearfix js-login">
-	<div>
-		<div>Please click below to log into to Google Calendar</div>
-	</div>
-	<div class="content">
-		<button class="ui fluid button"  onclick="getAuthorization()">Log in</button>
-	</div>
-</div>
-    			`);
-    		});
-        
+        // do api stuff
+        getWeather(queryParams.zipcode)
     }
 }
 
-function getAuthorization() {
-	$('.js-login').remove();
-	getGoogleCalAuth(false)
-		.then(() => loadCalendarData())
-		.then((events) => displayCalendarEvents(events))
-		.catch(() => {
-			// handle failure here :/
-			$('.js-container').append(`
-<div class="ui centered card clearfix js-login">
-	<div>
-		<div>Please click below to log into to Google Calendar</div>
-	</div>
-	<div class="content">
-		<button class="ui fluid button" onclick="getAuthorization()">Log in</button>
-	</div>
-</div>
-    		`);
-    	});	
-}
-
-
-function getDateAsText() {
-	let today = new Date();
-	const weekday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-	const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-	const w = weekday[today.getDay()];
-	const m = month[today.getMonth()];
-	const d = today.getDate();
-	const y = today.getFullYear();
-
-	today = `${w} ${m} ${d}, ${y}`;
-	
-	const displayTodaysDate = $('.todaysDate');
-	displayTodaysDate.text(today);	
-}
-
-// get google auth
-function getGoogleCalAuth(immediate) {
-	const CLIENT_ID = '790796644928-q7g8382df247mb9m2bmdt09m3s5qu3sh.apps.googleusercontent.com';
-    const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-	const permissionsOpts = {
-		client_id: CLIENT_ID,
-		scope: SCOPES,
-		immediate: immediate,
-	};
-
-	return new Promise((resolve, reject) => {
-		gapi.auth.authorize(permissionsOpts, (authResult) => {
-			if (authResult && !authResult.error) {
-				resolve();
-			} else {
-				reject();
-			}
-		});	
-	});
-}
-
-// function load google client
-function loadGoogleClient(clientName, version) {
-	return new Promise((resolve, reject) => {
-		gapi.client.load(clientName, version, () => {
-			resolve();
-		})
-	});
-}
-
-// get aclendar data
-function loadCalendarData() {
-	return loadGoogleClient('calendar', 'v3')
-		.then(() => listUpcomingEvents());
-}
-
-// get upcoming events
-function listUpcomingEvents() {
-	return new Promise((resolve, reject) => {
-
-		const request = gapi.client.calendar.events.list({
-          'calendarId': 'primary',
-          'timeMin': (new Date()).toISOString(),
-          'showDeleted': false,
-          'singleEvents': true,
-          'maxResults': 10,
-          'orderBy': 'startTime'
-        });
-
-        request.execute(function(resp) {
-        	resolve(resp.items)
-        });
-	});
-}
+// checkForZipcode();
 
 
 
 
+// =======================================================
+
+// $('.ui.fluid.large.teal.submit.button').click(onShowContentClicked);
+
+// function onShowContentClicked() {
+// 	$('.js-container').html(weatherCards.join('\n')+eventCards.join('\n'))	
+// };
+
+// get the zip code from the url
+console.log(location.search, location);
+
+
+
+let today = new Date();
+const weekday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const w = weekday[today.getDay()];
+const m = month[today.getMonth()];
+const d = today.getDate();
+const y = today.getFullYear();
+
+today = `${w} ${m} ${d}, ${y}`;
+console.log(today);
+const displayTodaysDate = $('.todaysDate');
+displayTodaysDate.text(today);
+
+
+
+// const event = {
+// 	eventName: 'Holiday Party',
+// 	time: '7pm',
+// 	location: 'The Standard (848 Washington St)',
+// }
+
+// const data = {
+// 	weather: {
+// 		degrees: '45 &#8457',
+// 		precip: '80% Precip.',
+// 	},
+// 	events: [
+// 		{
+// 			eventName: 'Holiday Party',
+// 			time: '7pm',
+// 			location: 'The Standard (848 Washington St)',
+// 		},
+// 		{
+// 			eventName: 'Yoga',
+// 			time: '8pm',
+// 			location: 'Yoga Herald Square (139 W. 35th St)',
+// 		},
+// 		{
+// 			eventName: 'Meeting',
+// 			time: '3:30pm',
+// 			location: 'Gin',
+// 		}
+// 	]
+// }
+
+// const events = [
+// 	{
+// 		eventName: 'Holiday Party',
+// 		time: '7pm',
+// 		location: 'The Standard (848 Washington St)',
+// 	},
+// 	{
+// 		eventName: 'Yoga',
+// 		time: '8pm',
+// 		location: 'Yoga Herald Square (139 W. 35th St)',
+// 	},
+// 	{
+// 		eventName: 'Meeting',
+// 		time: '3:30pm',
+// 		location: 'Gin',
+// 	}
+// ]
+
+// console.log('events: ', events);
+
+// console.log('events sub: ', data.events[2].eventName);
